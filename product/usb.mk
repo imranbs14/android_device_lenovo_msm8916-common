96d48a49154ad1c91c2816eec3202523ecea8504
dev-harsh1998 <harshit.india@linaro.org>
2017-08-26 19:29:11 +0530

    msm8916-common: o bringup
     This commit includes following changes:
    * Remove declarations of qcom hals and switch to Aosp hal
    * These HALs have to roasted from now: DRM, DISPLAY, VIBRATOR, POWER, USB, KEYMASTER
    * Update Display Flags for android o
    * Credits LUCA STEFANI

--- product/usb.mk
+++ product/usb.mk
@@ -2,6 +2,10 @@
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
