e3a7f707b2b1c7d7879ef837c648a29eda5f993e
dev-harsh1998 <harshit.india@linaro.org>
2018-03-25 22:43:48 +0530

    msm8916-common: ship custom healthd binary

--- product/charger.mk
+++ product/charger.mk
@@ -2,4 +2,5 @@
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@1.0-service \
+    chargeonlymode
