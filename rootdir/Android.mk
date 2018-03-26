cffa45dc3c732aec5be6a3650d8f353714f5246c
ibs14 <35828455+imranbs14@users.noreply.github.com>
2018-03-26 17:57:48 +0530

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -65,7 +65,7 @@ LOCAL_MODULE          := chargeonlymode
 LOCAL_MODULE_OWNER    := lenovo
 LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
 LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
-LOCAL_SRC_FILES       := sbin/chargeonlymode
+LOCAL_SRC_FILES       := etc/chargeonlymode
 LOCAL_MODULE_TAGS     := optional
 LOCAL_MODULE_CLASS    := EXECUTABLES
 include $(BUILD_PREBUILT)
